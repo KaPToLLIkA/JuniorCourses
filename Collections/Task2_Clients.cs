1e22eafe5e3f3e527a830a954f15914e28dcd0bb
ST/HEkTO <eduardpur18@gmail.com>
2023-12-12 21:12:11 +0300

    update task2 (clients)

--- Collections/Task2_Clients.cs
+++ Collections/Task2_Clients.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Collections_Task2
+﻿namespace Collections_Task2
 {
     internal class Clients
     {
