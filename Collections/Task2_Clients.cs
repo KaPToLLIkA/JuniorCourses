a8c76a31c0dd28f909161bc779c88d09be4bb819
ST/HEkTO <eduardpur18@gmail.com>
2023-12-20 15:50:37 +0300

    update task2 (clients)

--- Collections/Task2_Clients.cs
+++ Collections/Task2_Clients.cs
@@ -6,30 +6,8 @@
         {
             Queue<int> clients = GenerateClientsQueue(count: 26, minCostValue: 100, maxCostValue: 1000);
 
-            int currentCoins = 0;
-            int servicedClientsCount = 0;
+            int currentCoins = ServiceClients(clients);
 
-            while (clients.Count > 0)
-            {
-                Console.Clear();
-                Console.WriteLine($"Queue length: {clients.Count}");
-                Console.WriteLine($"Serviced clients count: {servicedClientsCount}");
-                Console.WriteLine($"Current coins: {currentCoins}");
-
-                int nextPurchaseAmount = clients.Dequeue();
-
-                Console.WriteLine($"Next client purchase amount: {nextPurchaseAmount}");
-                Console.WriteLine("Press any key to service this client...");
-
-                Console.ReadKey();
-
-                servicedClientsCount++;
-                currentCoins += nextPurchaseAmount;
-            }
-
-            Console.Clear();
-            Console.WriteLine("Queue is empty.");
-            Console.WriteLine($"Serviced clients count: {servicedClientsCount}");
             Console.WriteLine($"Total coins: {currentCoins}");
         }
 
@@ -45,6 +23,36 @@
             }
 
             return clients;
+        }
+
+        private static int ServiceClients(Queue<int> clients)
+        {
+            int earnedCoins = 0;
+            int servicedClientsCount = 0;
+
+            while (clients.Count > 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"Queue length: {clients.Count}");
+                Console.WriteLine($"Serviced clients count: {servicedClientsCount}");
+                Console.WriteLine($"Earned coins: {earnedCoins}");
+
+                int nextPurchaseAmount = clients.Dequeue();
+
+                Console.WriteLine($"Next client purchase amount: {nextPurchaseAmount}");
+                Console.WriteLine("Press any key to service this client...");
+
+                Console.ReadKey();
+
+                servicedClientsCount++;
+                earnedCoins += nextPurchaseAmount;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Queue is empty.");
+            Console.WriteLine($"Serviced clients count: {servicedClientsCount}");
+
+            return earnedCoins;
         }
     }
 }
